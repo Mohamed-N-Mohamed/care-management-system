8479c78389cc6770a8ce864065c646f9a346ff79
Mohamed-N-Mohamed <Mohamed-N-Mohamed@outlook.com>
2021-02-08 18:44:49 +0000

    https request completed

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-///
+//express
+const express = require("express");
+
+//dotenv
+const dotenv = require("dotenv");
+
+//morgan
+const morgan = require("morgan");
+
+//body parser
+const bodyparser = require("body-parser");
+
+const app = express();
+
+//Port
+const port = process.env.PORT || 3000;
+
+dotenv.config({ path: "config.env" });
+
+//parse request
+app.use(bodyparser.urlencoded({ extended: true }));
+
+//set view engine as ejs
+app.set("view engine", "ejs");
+
+//log requests
+app.use(morgan("tiny"));
+
+//load assets
+app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
+
+app.get("/", (req, res) => {
+  res.send("testing");
+});
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
