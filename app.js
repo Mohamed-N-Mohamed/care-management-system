5706cd9c83b3664b14eddc11b258e66901b15dad
Mohamed-N-Mohamed <Mohamed-N-Mohamed@outlook.com>
2021-02-10 21:27:13 +0000

    created route for ejs files

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const morgan = require("morgan");
 //body parser
 const bodyparser = require("body-parser");
 
+//path
+const path = require('path');
+
 const app = express();
 
 //Port
@@ -24,13 +27,25 @@ app.use(bodyparser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 //log requests
-app.use(morgan("tiny"));
+// app.use(morgan("tiny"));
 
 //load assets
 // app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
+app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
 
 app.get("/", (req, res) => {
   res.render("index");
 });
+
+
+app.get("/add", (req, res) => {
+  res.render("add-client");
+});
+
+app.get("/update", (req, res) => {
+  res.render("update-client");
+});
+
+
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
